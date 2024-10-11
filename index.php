f93392565ece454807a0faa251932ebf514bd945
Alessio Vietri <info@alessiovietri.it>
2024-10-11 12:39:48 +0200

    Completato LC

--- index.php
+++ index.php
@@ -1,45 +1,228 @@
 <?php
-    $columnsNumber = 4;
+
+// class SpecialData {
+
+//     public $name;
+//     public $email;
+
+// }
+
+// class User extends SpecialData {
+
+//     public $birthDate;
+
+// }
+
+// class Category {
+
+//     public $name;
+//     public $active;
+
+// }
+
+// class Company extends SpecialData {
+
+//     public $address;
+//     public $phone;
+
+// }
+
+// class WithName {
+
+//     protected $name;
+
+//     public function getName() {
+//         return $this->name;
+//     }
+
+//     public function setName(string $name) {
+//         if (strlen($name) >= 3) {
+//             $this->name = $name;
+//         }
+//         else {
+//             $this->name = null;
+//         }
+//     }
+
+// }
+
+// class WithEmail {
+
+//     protected $email;
+
+//     public function getEmail() {
+//         return $this->email;
+//     }
+
+//     public function setEmail(string $email) {
+//         if (strlen($email) >= 3) {
+//             $this->email = $email;
+//         }
+//         else {
+//             $this->email = null;
+//         }
+//     }
+
+// }
+
+// class User extends WithName {
+
+//     public $email;
+//     public $birthDate;
+
+// }
+
+// class Category extends WithName {
+
+//     public $active;
+
+// }
+
+// class Company extends WithName {
+
+//     public $email;
+//     public $address;
+//     public $phone;
+
+// }
+
+trait HasName {
+
+    protected $name;
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName(string $name) {
+        if (strlen($name) >= 3) {
+            $this->name = $name;
+        }
+        else {
+            $this->name = null;
+        }
+    }
+
+}
+
+trait HasEmail {
+
+    protected $email;
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail(string $email) {
+        if (
+            strlen($email) >= 5
+            &&
+            strlen($email) < 256
+        ) {
+            $this->email = $email;
+        }
+        else if (strlen($email) == 1) {
+            throw new EmailTooShortError('email troppo corta. Deve essere almeno 5 caratteri');
+        }
+        else {
+            $this->email = null;
+        }
+    }
+
+}
+
+class EmailTooShortError extends Exception {
+
+}
+
+class BirthDateError extends Exception {
+
+}
+
+class User {
+
+    use HasName;
+    use HasEmail;
+
+    public $birthDate;
+
+    function __construct(string $name, string $email, string $birthDate) {
+        $this->setName($name);
+        $this->setEmail($email);
+
+        if (
+            is_string($birthDate)
+            &&
+            strlen($birthDate) == 10
+        ) {
+            $this->birthDate = $birthDate;
+        }
+        else {
+            throw new BirthDateError('data di nascita non valida. Meno di 10 caratteri passati');
+        }
+    }
+
+}
+
+class Category {
+
+    use HasName;
+
+    public $active;
+
+}
+
+class Company {
+
+    use HasName;
+    use HasEmail;
+    
+    public $address;
+    public $phone;
+
+}
+
+try {
+    $user = new User('Mario', 'mario@boolean.careers', $_GET['birthDate']);
+
+    var_dump($user);
+}
+catch(EmailTooShortError $ciccio) {
+    var_dump($ciccio);
+    echo '<h4 style="color: red;">ERRORE EMAIL: '.$ciccio->getMessage().'</h4>';
+}
+catch(BirthDateError $e) {
+    var_dump($e);
+    echo '<h4 style="color: red;">ERRORE DATA DI NASCITA: '.$e->getMessage().'</h4>';
+}
+catch(Exception $e) {
+    echo '<h4 style="color: red;">ERRORE: '.$e->getMessage().'</h4>';
+}
+
+$category = new Category();
+$company = new Company();
+
+var_dump($category);
+var_dump($company);
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
 
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    </head>
-    <body>
-        
-        <header class="py-4 mb-4">
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center">
-                        <h1>
-                            Template PHP
-                        </h1>
-                    </div>
-                </div>
-            </div>
-        </header>
+<script>
 
-        <main>
-            <div class="container">
-                <div class="row">
-                    <?php
-                        for ($i = 0; $i < $columnsNumber; $i++) {
-                    ?>
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                            Colonna <?php echo $i + 1; ?>
-                        </div>
-                    <?php
-                        }
-                    ?>
-                </div>
-            </div>
-        </main>
+    axios
+        .get('https://google.com')
+        .then(res => {
+            // ...
+        })
+        .catch(err => {
+            // Gestisco l'errore...
+        });
 
-    </body>
-</html>
+    try {
+        const num = 'ciao' * 4;
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+</script>
